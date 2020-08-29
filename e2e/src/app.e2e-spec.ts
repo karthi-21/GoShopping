1c00a58d6a6f0894590a65e5811f4cdda9514bc0
karthikeyansekar <karthikeyansekarc@gmail.com>
2020-08-29 15:08:06 +0530

    name change

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('e-commerce-poc app is running!');
+    expect(page.getTitleText()).toEqual('go-shopping app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'e-commerce-poc'`, () => {
+  it(`should have as title 'go-shopping'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('e-commerce-poc');
+    expect(app.title).toEqual('go-shopping');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('e-commerce-poc app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('go-shopping app is running!');
   });
 });
